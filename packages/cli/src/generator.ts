6e7f33c910005c6eaed41341e3a7f164475aad92
Julie <147175740+JulieSeyier@users.noreply.github.com>
2026-01-13 17:11:49 +0100

    :sparkles: Ajout de la possibilité de changer la taille des titres

--- packages/cli/src/generator.ts
+++ packages/cli/src/generator.ts
@@ -98,9 +98,11 @@ export class SlideDeckGenerator {
             : '';
 
         const titleHtml = slide.title
-            ? `<h2 class="slide-title">${sanitizeTextContainerHtml(slide.title)}</h2>
+        ? `<h2 class="slide-title"${slide.titleSize ? ` style="font-size: ${slide.titleSize};"` : ''}>
+            ${sanitizeTextContainerHtml(slide.title)}
+        </h2>
         <hr class="slide-separator">`
-            : '';
+        : '';
 
         const slideNumberHtml = slideNumber !== undefined
             ? `<div class="slide-number">${slideNumber}</div>`
