cb7586ae6a677dfd516928a33e709ad9adbcb6d9
theoLassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2026-01-17 17:21:22 +0100

    refactor: refactor code structure after merging main

--- packages/cli/src/containers/containers.ts
+++ packages/cli/src/containers/containers.ts
@@ -1,8 +1,8 @@
-import { CodeContainer, TextContainer, Container, MediaContainer } from '../../../language/out/generated/ast.js';
-import { generateAnimationAttributes, generateEquationControls, generateEquationDescription, generateEquationLines } from '../math/math.js';
+import { CodeContainer, TextContainer, Container, MediaContainer, MathContainer } from '../../../language/out/generated/ast.js';
 import { generateTextContainer,generateTextContainerDefaultStyle } from './TextContainer.js';
 import { generateMediaContainer, generateMediaContainerDefaultStyle } from './MediaContainer.js';
 import { generateCodeContainer, generateCodeContainerDefaultStyle } from './CodeContainer.js';
+import { generateMathContainer } from './MathContainer.js';
 
 export function generateContainer(container: Container): string {
     switch (container.$type) {
@@ -25,23 +25,4 @@ export function generatecontainersStyle(): string {
     ${generateTextContainerDefaultStyle()}
     ${generateCodeContainerDefaultStyle()}
 `;
-}
-
-let equationCounter = -1;
-
-export function generateMathContainer(container: MathContainer) {
-    equationCounter++;
-    const equationId = `eq-${equationCounter}`;
-
-    const animationsAttributes = generateAnimationAttributes(container);
-    const animationsMarker = animationsAttributes ? `data-animations="${animationsAttributes}"`
-        : '';
-
-    return `
-            <div class="equation-wrapper" data-equation-id="${equationId}" ${animationsMarker}>
-                ${generateEquationLines(container.equationLines, equationId)}
-                ${generateEquationControls()}
-                ${container.description ? generateEquationDescription(container.description) : ''}
-            </div>
-    `;
 }
