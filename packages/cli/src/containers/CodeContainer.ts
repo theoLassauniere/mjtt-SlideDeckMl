402b2cd1e4708aa7ec1f24c0a16ca08d4a7b3c76
theoLassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2026-01-18 01:37:46 +0100

    feat: added steps in code container

--- packages/cli/src/containers/CodeContainer.ts
+++ packages/cli/src/containers/CodeContainer.ts
@@ -3,8 +3,15 @@ import { CodeContainer } from '../../../language/out/generated/ast.js';
 export function generateCodeContainer(codeContainer: CodeContainer) {
     const codeLength = codeContainer.code.length;
     const cleaned = codeContainer.code.substring(3, codeLength - 4).trim();
+    let steps = '';
+
+    if (codeContainer.steps) {
+        // gadre que les steps
+        steps = "=\"" + codeContainer.steps.slice(1, -1) + "\"";
+    }
+
     return `
-            <pre><code class="langage-${codeContainer.language.toLowerCase()}" data-trim data-line-numbers>
+            <pre><code class="langage-${codeContainer.language.toLowerCase()}" data-trim data-line-numbers${steps}>
 ${cleaned}
             </code></pre>
         `
