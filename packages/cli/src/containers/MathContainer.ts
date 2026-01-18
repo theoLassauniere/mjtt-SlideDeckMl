ca4197b785271120454a570561cd2f490a685b1f
thibo24 <thibault.ripoche@free.fr>
2026-01-18 12:59:23 +0100

    rebase with main

--- packages/cli/src/containers/MathContainer.ts
+++ packages/cli/src/containers/MathContainer.ts
@@ -1,18 +1,20 @@
 import { MathContainer } from "slide-deck-ml-language";
 import { generateAnimationAttributes, generateEquationControls, generateEquationDescription, generateEquationLines } from "../math/math.js";
+import  { containerOptionsToFragment } from "../utils/utils.js";
 
 let equationCounter = -1;
 
 export function generateMathContainer(container: MathContainer) {
     equationCounter++;
     const equationId = `eq-${equationCounter}`;
+    const fragment = containerOptionsToFragment(container.options);
 
     const animationsAttributes = generateAnimationAttributes(container);
     const animationsMarker = animationsAttributes ? `data-animations="${animationsAttributes}"`
         : '';
 
     return `
-            <div class="equation-wrapper" data-equation-id="${equationId}" ${animationsMarker}>
+            <div class="equation-wrapper ${fragment.className}" ${fragment.attrs} data-equation-id="${equationId}" ${animationsMarker}>
                 ${generateEquationLines(container.equationLines, equationId)}
                 ${generateEquationControls()}
                 ${container.description ? generateEquationDescription(container.description) : ''}
